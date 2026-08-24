277f1f7db12f1242d1a8ac9aeeab5310263d4bf7
mdfpva <mdfpva@protonmail.com>
2026-08-24 15:24:13 +0100

    added codexion.c

--- codexion.c
+++ codexion.c
@@ -6,7 +6,7 @@
 /*   By: mide-fre <mide-fre@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/21 16:01:05 by mide-fre          #+#    #+#             */
-/*   Updated: 2026/08/21 16:03:16 by mide-fre         ###   ########.fr       */
+/*   Updated: 2026/08/24 15:23:17 by mide-fre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,17 @@
 
 int	main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
+	t_sim	sim;
+
+	memset(&sim, 0, sizeof(t_sim));
+	if (!parse_args(&sim, ac, av))
+		return (1);
+	if (!init_sim(&sim))
+	{
+		cleanup_sim(&sim);
+		return (1);
+	}
+	run_sim(&sim);
+	cleanup_sim(&sim);
 	return (0);
 }
